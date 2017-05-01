904044b29322505ceb141b019a8de632444e5b9e
Reilly Bergin-Pugh <rberginpugh@gmail.com>
2017-05-01 19:42:52 -0400

    Ruby hashes and symboles tested

---
+++ store_item.rb
@@ -0,0 +1,12 @@
+foodstuffs1 = {:name => 'kiwis', :color => 'brown', :price => 3} 
+foodstuffs2 = {:name => 'starfruits', :color => 'yellow', :price => 5} 
+foodstuffs3 = {:name => 'persimmons', :color => 'orange', :price => 2} 
+
+foodstuffs1 = {name: 'kiwis', color: 'brown', price: 3} 
+foodstuffs2 = {name: 'starfruits', color:'yellow', price: 5} 
+foodstuffs3 = {name: 'persimmons', color: 'orange', price: 2} 
+
+
+puts "At my store, #{foodstuffs1[:name]} are a wonderful shade of #{foodstuffs1[:color]} and only cost $#{foodstuffs1[:price]} USD!"
+puts "At my store, #{foodstuffs2[:name]} are a wonderful shade of #{foodstuffs2[:color]} and only cost $#{foodstuffs2[:price]} USD!"
+puts "At my store, #{foodstuffs3[:name]} are a wonderful shade of #{foodstuffs3[:color]} and only cost $#{foodstuffs3[:price]} USD!"
